287746693c87f201137c6f84a63a9d595394c921
Latouche Gerson <latouche822@gmail.com>
2025-08-31 22:03:31 -0400

    up0909

--- src/app/components/hss/hero-section/hero-section.ts
+++ src/app/components/hss/hero-section/hero-section.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
     <section class="hero-wrapper">
         <div class="box-wrapper">
             <div class="box">
-                <h1 style="font-size: 60px; font-weight: 900; color: #262626;">Your vision, Our expertise</h1>
+                <h1 style="font-size: clamp(1.8rem,4vw,4rem); font-weight: 900; color: #262626;">Your vision, Our expertise</h1>
                 <p style="color: #575757ff;">Outsourcing support services with SECO to enhance your facilities operations. We provide expert solutions that streamline processes and improve service quality.</p>
 
             </div>
@@ -15,7 +15,7 @@ import { Component } from '@angular/core';
                 <path class="stroke-animation" pathLength="1" id="thePath" d="M265 39V4H4V506H264.32V491.071" stroke="#ED2939" stroke-width="8"></path>
             </svg>
         </div>
-        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="695px" height="671px" viewBox="0 0 695 671" style="border:1px solid transparent;  ">
+        <svg class="slider-svg" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="695px" height="671px" viewBox="0 0 695 671" style="border:1px solid transparent;  ">
                 <defs>
                         <clipPath id="clip-path-right" transform="translate(310.000000, 0.000000)">
                             <path fill="none" d="M384.798555,0 L104,0 C34.6666667,165.333333 5.68434189e-14,305.333333 5.68434189e-14,420 C5.68434189e-14,534.666667 35.6666667,618.333333 107,671 L384.798555,671 C316.402332,608.699318 283.136147,521.044866 285,408.036646 C286.863853,295.028425 320.130038,159.01621 384.798555,0 Z"></path>
